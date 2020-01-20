eb8ce853b09940a1cf2ca8a88513a3e3b38d0b3c
tariktabakovic <tariktab97@gmail.com>
2020-01-20 00:52:38 -0500

    adds bodyparser and user sign up form

--- index.js
+++ index.js
@@ -1,63 +1,67 @@
 const http = require('http');
 const express = require('express');
-const app = express();
+
 const PORT = 3007;
 
-const session = require("express-session");
-const FileStore = require("session-file-store")(session);
+// const session = require("express-session");
+// const FileStore = require("session-file-store")(session);
 
-app.use(session({
-    store: new FileStore({}),
-    secret: ""
-}));
-
-app.use((req, res, next)=>{
-    console.log(req.session);
-    next();
-});
-
-const es6renderer = require('express-es6-template-engine');
-app.engine('html', es6renderer);
-app.set('views', 'templates');
-app.set('view engine', 'html');
 
 const bodyParser = require('body-parser');
 const parseForm = bodyParser.urlencoded({
     extended: true
 });
 
+const app = express();
+
+const es6renderer = require('express-es6-template-engine');
+app.engine('html', es6renderer);
+app.set('views', 'templates');
+app.set('view engine', 'html');
+
+
+
 const server = http.createServer(app);
 
 const users = require('./models/users');
 
-app.get('/', async (req, res)=>{
-    res.render('home')
-})
+// app.get('/', async (req, res)=>{
+//     res.render('home')
+// })
 
-app.post('/', async (req, res)=>{
+// app.post('/', async (req, res)=>{
     
-    
-    res.redirect('users/auth')
+//     res.redirect('users/auth')
+// });
+
+
+// function requireLogin(req, res, next){
+//     if(req.session && req.session.user){
+//         console.log('require login says you are good');
+//         next();
+//     } else {
+//         console.log('incorrect username or password');
+//         res.redirect('users/auth');
+//     }
+// };
+
+app.get('/signup', (req, res)=>{
+    console.log('yes you are at the sign up');
+    res.render('users/signup', {
+        locals: {
+            username: '',
+            password: '',
+        }
+    });
 });
 
-
-function requireLogin(req, res, next){
-    if(req.session && req.session.user){
-        console.log('require login says you are good');
-        next();
-    } else {
-        console.log('incorrect username or password');
-        res.redirect('users/auth');
-    }
-};
-
-app.get('/signup', async (req, res)=>{
-    res.render('users/signup');
-})
-
-app.get('/signup', async (req, res)=>{
+app.post('/signup', parseForm, async (req, res)=>{
+    const { username , password } = req.body
+    const result = await users.create(username, password);
+    console.log(result);
+    res.end();
     res.redirect('users/auth');
-})
+});
 
 app.get('/login', async (req,res) => {
     res.render('users/auth');
@@ -79,30 +83,31 @@ app.post('/login', parseForm, async (req, res) =>{
     }else {
         console.log('Incorrect username or password.')
     } 
+    res.end();
 });
 
 
-app.get('/games', requireLogin, async (req, res) =>{
+// app.get('/games', requireLogin, async (req, res) =>{
 
-})
+// })
 
-app.post('/games', requireLogin, async (req, res) =>{
-    res.redirect('/chatrooms')
-})
+// app.post('/games', requireLogin, async (req, res) =>{
+//     res.redirect('/chatrooms')
+// })
 
-app.get('/chatrooms', requireLogin, async (req, res) =>{
+// app.get('/chatrooms', requireLogin, async (req, res) =>{
 
-})
+// })
 
-app.post('/chatrooms', requireLogin, async (req, res) =>{
+// app.post('/chatrooms', requireLogin, async (req, res) =>{
 
-})
+// })
 
-app.get('/logout', requireLogin, async (req, res)=>{
-    req.session.destroy(()=>{
-        res.redirect('/login')
-    });
-});
+// app.get('/logout', requireLogin, async (req, res)=>{
+//     req.session.destroy(()=>{
+//         res.redirect('/login')
+//     });
+// });
 
 server.listen(PORT, ()=>{
     console.log(`listening on ${PORT}`)
